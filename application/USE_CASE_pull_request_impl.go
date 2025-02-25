29380e8ee841289abf43722824cd2b94937f0931
IDS-HUGO <hugofranciscoluis@gmail.com>
2025-02-25 11:56:05 -0600

    PRUEBA 4

--- application/USE_CASE_pull_request_impl.go
+++ application/USE_CASE_pull_request_impl.go
@@ -18,9 +18,12 @@ func ProcessPullRequest(payload []byte) int {
 		branch := eventPayload.PullRequest.Head.Ref
 		user := eventPayload.PullRequest.User.Login
 		pRID := eventPayload.PullRequest.ID
+
 		log.Printf("Pull Request Recibido:\nID:%d\nBase:%s\nHead:%s\nUser:%s", pRID, base, branch, user)
+
 	} else {
 		log.Printf("Pull Request Action no es Closed: %s", eventPayload.Action)
+
 	}
 
 	return 200
